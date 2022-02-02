942f2fa5cd462121b2d673ec4935aa2d34967d3b
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2022-02-02 21:30:15 +0100

    erc-20-init

---
+++ erc-20-token/test/index.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+
+
+describe("Token", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    expect('1').to.equal('1')
+  });
+});
