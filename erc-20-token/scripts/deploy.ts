fcea55f98a327de149b39ef78f7bb5a8a305ed3c
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2022-02-03 13:17:51 +0100

    create token itself

--- erc-20-token/scripts/deploy.ts
+++ erc-20-token/scripts/deploy.ts
@@ -16,9 +16,9 @@ async function main() {
   // We get the contract to deploy
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
-
+ 
   await token.deployed();
-
+ 
   console.log("Token deployed to:", token.address);
 }
 
