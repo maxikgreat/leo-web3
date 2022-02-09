74e2de21f49e1580cb92b9882eeecc3b05fe46f1
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2022-02-09 15:15:01 +0100

    handle buy and withdrawToOwner functions

--- erc-20-token/contracts/Token.sol
+++ erc-20-token/contracts/Token.sol
@@ -1,21 +1,40 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
 contract Token {
+  struct Vesting {
+    uint256 unblockTime;
+    uint256 tokens;
+  }
+
   string public name = 'Leocode Token';
   string public symbol = 'LEO';
   uint8 public decimals = 18;
   uint256 public totalSupply = 100000000000000000000000;
+
+  address public owner;
+  uint8 public ethRate = 100;
+  uint32 public vestingPeriod = 7 days;
+
   mapping(address => uint256) public balanceOf;
   mapping(address => mapping(address => uint256)) public allowance;
+  mapping(address => Vesting) public vesting;
 
   modifier nonZeroAddress(address _address) {
     require(_address != address(0), 'Address 0x0 is not allowed');
     _;
   }
 
-  modifier sufficientSenderBalance(address _sender, uint _value) {
-    require(balanceOf[_sender] >= _value, 'Not enough tokens to transfer');
+  modifier sufficientSenderBalance(address _sender, uint256 _value) {
+    uint256 freeTokens = balanceOf[_sender];
+
+    if (vesting[_sender].unblockTime > block.timestamp) {
+      freeTokens -= vesting[_sender].tokens;
+    } else {
+      vesting[msg.sender] = Vesting(0, 0);
+    }
+
+    require(freeTokens >= _value, 'Not enough tokens to transfer');
     _;
   }
 
@@ -23,13 +42,31 @@ contract Token {
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
   constructor() {
-    balanceOf[msg.sender] = totalSupply;
+    owner = msg.sender;
+    balanceOf[address(this)] = totalSupply;
+  }
+
+  function withdrawToOwner() external {
+    require(msg.sender == owner, 'You have no rights to do this operation');
+    (bool success,) = owner.call{value: address(this).balance}('');
+    require(success, 'Failed to send ether');
   }
 
   function approve(address _spender, uint256 _value) public nonZeroAddress(_spender) returns (bool) {
     allowance[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
     return true;
+  }
+
+  function buy() external payable returns (uint256) {
+    require(vesting[msg.sender].unblockTime <= block.timestamp, 'You\'ve already bought tokens on this week');
+    require(msg.value > 0, 'Min eth value to buy tokens is 1 wei');
+    uint256 tokens = msg.value * ethRate;
+    require(tokens <= totalSupply, 'Not enough tokens to buy');
+    balanceOf[msg.sender] += tokens;
+    balanceOf[address(this)] -= tokens;
+    vesting[msg.sender] = Vesting(block.timestamp + vestingPeriod, tokens);
+    return tokens;
   }
 
   function transfer(address _to, uint256 _value) public
