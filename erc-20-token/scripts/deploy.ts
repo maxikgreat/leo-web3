7e0550951bd2101ba5ea2cd0846ab2f407b3b076
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2022-02-10 11:09:36 +0100

    add read.me

--- erc-20-token/scripts/deploy.ts
+++ erc-20-token/scripts/deploy.ts
@@ -3,7 +3,28 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { artifacts, ethers } from "hardhat";
+import { Contract } from "ethers";
+import fs from "fs";
+import path from "path";
+
+function saveFrontendFiles(token: Contract) {
+  const contractDir = path.join(__dirname, '..', '..', 'leo-swap', 'src', 'contract');
+  
+  if (!fs.existsSync(contractDir)) {
+    fs.mkdirSync(contractDir)
+  }
+  
+  fs.writeFileSync(
+    path.join(contractDir, 'addresses.json'),
+    JSON.stringify({Token: token.address}, undefined, 2)
+  )
+  
+  fs.writeFileSync(
+    path.join(contractDir, 'Token.json'),
+    JSON.stringify(artifacts.readArtifactSync('Token'), null, 2)
+  )
+}
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -16,10 +37,12 @@ async function main() {
   // We get the contract to deploy
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
- 
+  
   await token.deployed();
  
   console.log("Token deployed to:", token.address);
+  
+  saveFrontendFiles(token)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
